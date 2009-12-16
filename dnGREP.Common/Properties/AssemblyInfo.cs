d87f87681fdef279fe54513749f8eecdeb7366c1
denis stankovski <denis.stankovski@60b9a3ae-a9c9-11dd-b08a-b13dd1f1ca2d>
2009-12-16 19:07:43 +0000

    * Re-organized folder structure

--- dnGREP.Common/Properties/AssemblyInfo.cs
+++ dnGREP.Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dnGREP.Common")]
+[assembly: AssemblyTitle("dnGREP.Common1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BAH")]
-[assembly: AssemblyProduct("dnGREP.Common")]
+[assembly: AssemblyProduct("dnGREP.Common1")]
 [assembly: AssemblyCopyright("Copyright © BAH 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1be8200e-00d8-468c-bfdf-a1385bf7244a")]
+[assembly: Guid("0f9a83f7-d793-477c-8d40-91293d67a793")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
