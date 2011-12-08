565489b801fce621c42014e4e25d8e47a2466a57
denis stankovski <denis.stankovski@gmail.com>
2011-12-08 16:07:46 -0500

    Added strong name to all main assemblies Added ability to pre-start app

---
+++ dnGREP.WPF/Launcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace dnGREP.WPF
+{
+    public class Launcher : WindowsFormsApplicationBase
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        { 
+            (new Launcher()).Run(args); 
+        }
+
+        public Launcher()
+        { 
+            IsSingleInstance = true; 
+        }
+
+        public dnGrepApp App { get; private set; }
+
+        protected override bool OnStartup(StartupEventArgs e)
+        {
+            App = new dnGrepApp();
+
+            if (e.CommandLine != null && e.CommandLine.Count > 0 && e.CommandLine[0] == "/hidden")
+            {
+                App.Run();
+            }
+            else
+            {
+                App.Run();
+                App.ProcessArgs(e.CommandLine.ToArray(), true);
+            }
+            return false;
+        }
+
+        protected override void OnStartupNextInstance(
+          StartupNextInstanceEventArgs eventArgs)
+        {
+            base.OnStartupNextInstance(eventArgs);
+            App.ProcessArgs(eventArgs.CommandLine.ToArray(), false);
+        }
+    }
+}
---
+++ dnGREP.WPF/dnGrepApp.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using dnGREP.Common;
+using NLog;
+using System;
+
+namespace dnGREP.WPF
+{
+    public class dnGrepApp : Application
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public MainForm MainForm { get; private set; }
+
+        public dnGrepApp()
+            : base()
+        {
+            this.DispatcherUnhandledException += new System.Windows.Threading.DispatcherUnhandledExceptionEventHandler(dnGrepApp_DispatcherUnhandledException);
+            Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("Styles.xaml", UriKind.Relative) });
+        }
+
+        void dnGrepApp_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            logger.LogException(LogLevel.Error, e.Exception.Message, e.Exception);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Utils.DeleteTempFolder();
+            MainForm = new MainForm();
+        }
+
+        public void ProcessArgs(string[] args, bool firstInstance)
+        {
+            if (args != null && args.Length > 0 && args[0] != "/hidden")
+            {
+                string searchPath = args[0];
+                if (searchPath.EndsWith(":\""))
+                    searchPath = searchPath.Substring(0, searchPath.Length - 1) + "\\";
+                GrepSettings.Instance.Set<string>(GrepSettings.Key.SearchFolder, searchPath);
+            }
+            MainForm.Show();
+            MainForm.Activate();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            Utils.DeleteTempFolder();
+        }
+    }
+}
