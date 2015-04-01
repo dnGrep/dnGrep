9b4ec32ce5e8300d6be024cf6ee2d651ef7ad0eb
John Burak <JVimes@users.noreply.github.com>
2015-03-31 21:20:19 -0600

    Making the WiX project's ProductVersion (now in Variables.wxi) automatically follow the AssemblyVersion from AssemblyVersionCommon.cs (which is now the canonical spot to set the product version number).

---
+++ dnGREP.Setup/PostBuild_dnGrepSetup.ps1
@@ -0,0 +1,20 @@
+﻿param(
+    #Path to AssemblyVersionCommon.cs
+    $AssemblyVersionCommonCSPath,
+    #Path to .msi
+    $TargetPath,
+    #Path to .wixpdb
+    $TargetPdbPath
+)
+Import-Module "$PSScriptRoot\ParseVersion.psm1" -Force
+
+Write-Host "Renaming .msi and .wipdb to have vesion number in filenames"
+function rename($filePath, $version)
+{
+    $file = Get-Item $filePath
+    $newFileName = ( ($file.BaseName).replace('X.X.X.X',"$($version.MajorVersion).$($version.MinorVersion).$($version.BuildVersion).$($version.Revision)") + $file.Extension )
+    Move-Item $filePath (Join-Path $file.Directory.FullName $newFileName)
+}
+$version = ParseVersion $AssemblyVersionCommonCSPath
+rename $TargetPath $version
+rename $TargetPdbPath $version
