cd33950105bc6635303fee9cc0ed43dfddd70e1b
Doug Persons <djpersons@gmail.com>
2019-03-23 15:43:58 -0700

    Update installer with two new dlls

--- AssemblyVersionCommon.cs
+++ AssemblyVersionCommon.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // by using '*'
 //
 // NOTE: Given our build, this is the canonical spot to set the product version number.
-[assembly: AssemblyVersion("2.9.101.0")]
-[assembly: AssemblyFileVersion("2.9.101.0")]
+[assembly: AssemblyVersion("2.9.128.0")]
+[assembly: AssemblyFileVersion("2.9.128.0")]
