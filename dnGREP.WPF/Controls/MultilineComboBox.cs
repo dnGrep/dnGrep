a2f9909794fbed38da4fac47741758a44a428bb1
Doug P <djpersons@gmail.com>
2020-03-01 11:54:05 -0800

    Add ComboBox override to pass up/down arrow keys to the text box when in multi-line mode (#403)

---
+++ dnGREP.WPF/Controls/MultilineComboBox.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace dnGREP.WPF
+{
+    /// <summary>
+    /// Overrides the ComboBox to send Up/Down arrow keys to the text box when in multi-line mode
+    /// </summary>
+    public class MultilineComboBox : ComboBox
+    {
+        private TextBox editTextBox;
+
+        public MultilineComboBox()
+            : base()
+        {
+            Loaded += EditableComboBox_Loaded;
+        }
+
+        private void EditableComboBox_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (this.Template.FindName("PART_EditableTextBox", this) is TextBox tb)
+            {
+                editTextBox = tb;
+            }
+        }
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            // AcceptsReturn means the text box is in multi-line mode
+            if (editTextBox != null && editTextBox.AcceptsReturn)
+            {
+                if (e.Key == Key.Up || e.Key == Key.Down)
+                {
+                    SendKey(e.Key);
+                    e.Handled = true;
+                    return;
+                }
+            }
+
+            base.OnPreviewKeyDown(e);
+        }
+
+        private void SendKey(Key key)
+        {
+            if (editTextBox != null)
+            {
+                editTextBox.RaiseEvent(
+                  new KeyEventArgs(Keyboard.PrimaryDevice, PresentationSource.FromVisual(editTextBox), 0, key)
+                  { 
+                      RoutedEvent = Keyboard.KeyDownEvent 
+                  });
+            }
+        }
+    }
+}
