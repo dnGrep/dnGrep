491eaedcaa857e4d163ff3708950639aee14662d
doug24 <djpersons@gmail.com>
2020-04-11 11:11:59 -0700

    Moved HookProc to base class Add HookProc to Floating window

--- DockFloat/FloatWindow.cs
+++ DockFloat/FloatWindow.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace DockFloat
 {
@@ -25,6 +27,32 @@ namespace DockFloat
             Content = content;
 
             Closing += FloatWindow_Closing;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            if (PresentationSource.FromVisual(this) is HwndSource hwndSource)
+            {
+                hwndSource.AddHook(HookProc);
+            }
+        }
+
+        private IntPtr HookProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == 0x0084 /*WM_NCHITTEST*/ )
+            {
+                // This prevents a crash in WindowChromeWorker._HandleNCHitTest
+                try
+                {
+                    lParam.ToInt32();
+                }
+                catch (OverflowException)
+                {
+                    handled = true;
+                }
+            }
+            return IntPtr.Zero;
         }
 
         public override void OnApplyTemplate()
