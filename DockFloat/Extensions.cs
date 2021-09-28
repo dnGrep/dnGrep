e077cfddd9e97fd86cc1a745bebbda754517c7e7
doug24 <djpersons@gmail.com>
2021-09-28 16:32:45 -0700

    fix for date picker watermark localization

--- DockFloat/Extensions.cs
+++ DockFloat/Extensions.cs
@@ -25,6 +25,20 @@ namespace DockFloat
             }
         }
 
+        public static T GetChildOfType<T>(this DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) return null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+
+                var result = (child as T) ?? GetChildOfType<T>(child);
+                if (result != null) return result;
+            }
+            return null;
+        }
+
         public static Point ToDevicePixels(this Window window, Point pt)
         {
             var t = PresentationSource.FromVisual(window).CompositionTarget.TransformToDevice;
