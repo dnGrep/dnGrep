3cc28f5e9338adc6a0698e3e5e96f5587fc5660d
doug24 <17227248+doug24@users.noreply.github.com>
2023-02-12 16:00:14 -0800

    fix platform support warnings

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -1,5 +1,6 @@
 // Common assembly information (other than version information, which is in another common file). http://stackoverflow.com/a/3341767
 using System.Reflection;
+using System.Runtime.Versioning;
 
 // AssemblyCompany name is used for AppData\Local\ directory name
 [assembly: AssemblyCompany("dnGrep")]
@@ -7,3 +8,4 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright 2013, 2023 dnGrep Community Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: SupportedOSPlatform("windows")]
