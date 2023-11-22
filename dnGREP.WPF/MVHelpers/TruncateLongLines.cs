76ace6ed0f4cd9141c796fcbfb39f0552018afc9
doug24 <17227248+doug24@users.noreply.github.com>
2023-11-22 12:57:32 -0800

    #992 - add class to stop rendering lines over 8K characters to work around performance bug in wpf

---
+++ dnGREP.WPF/MVHelpers/TruncateLongLines.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Media;
+using dnGREP.Localization;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Rendering;
+
+namespace dnGREP.WPF
+{
+    public class TruncateLongLines : VisualLineElementGenerator
+    {
+        private const int maxLength = 8000;
+        private const int charactersAfterEllipsis = 0;
+        private readonly string ellipsis = "•••";
+
+        public override int GetFirstInterestedOffset(int startOffset)
+        {
+            DocumentLine line = CurrentContext.VisualLine.LastDocumentLine;
+            if (line.Length > maxLength)
+            {
+                int ellipsisOffset = line.Offset + maxLength - charactersAfterEllipsis - ellipsis.Length;
+                if (startOffset <= ellipsisOffset)
+                    return ellipsisOffset;
+            }
+            return -1;
+        }
+
+        public override VisualLineElement ConstructElement(int offset)
+        {
+            FormattedText formattedText = new(ellipsis,
+                TranslationSource.Instance.CurrentCulture,
+                CurrentContext.TextView.FlowDirection,
+                CurrentContext.GlobalTextRunProperties.Typeface,
+                CurrentContext.GlobalTextRunProperties.FontRenderingEmSize,
+                Brushes.Firebrick,
+                CurrentContext.GlobalTextRunProperties.PixelsPerDip);
+
+            return new FormattedTextElement(formattedText, CurrentContext.VisualLine.LastDocumentLine.EndOffset - offset - charactersAfterEllipsis);
+        }
+    }
+}
