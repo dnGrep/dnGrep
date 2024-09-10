65e17514195b549c7a56a2c06fa41eadeaa09334
doug24 <17227248+doug24@users.noreply.github.com>
2024-09-09 17:22:40 -0700

    change path to makeappx.exe add debug message

--- make-msix.ps1
+++ make-msix.ps1
@@ -2,6 +2,8 @@ param( [string]$workingDirectory = $PSScriptRoot )
 Write-Host 'Executing Powershell script make-msix.ps1 with working directory set to: ' $workingDirectory
 Set-Location $workingDirectory
 
+Get-ChildItem "C:\Program Files (x86)\Windows Kits\10\bin" | Select Name
+
 $version = $env:APPVEYOR_BUILD_VERSION
 Write-Host 'New version is: ' $version
 
@@ -11,11 +13,11 @@ $doc = [System.Xml.Linq.XDocument]::Load($path)
 $xName = [System.Xml.Linq.XName]"{http://schemas.microsoft.com/appx/manifest/foundation/windows10}Identity"
 $doc.Root.Element($xName).Attribute("Version").Value = $version;
 $doc.Save($path)
-Write-Host 'Updated verion in ' $path
+Write-Host 'Updated version in ' $path
 
 $packagePath = $workingDirectory + "/dnGREP.ContextMenuPkg"
 $msixPath = $workingDirectory + "/dnGREP.msix"
 #$args = "pack /o /d `"$packagePath`" /p `"$msixPath`" /nv"
-& 'C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\makeappx.exe' @('pack', '/o', '/d', $packagePath, '/p', $msixPath, '/nv')
+& 'C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\makeappx.exe' @('pack', '/o', '/d', $packagePath, '/p', $msixPath, '/nv')
 
 Write-Host 'After makeappx'
