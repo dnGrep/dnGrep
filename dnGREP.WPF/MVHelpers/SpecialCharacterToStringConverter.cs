3037ddc55286cc663c9dd8a47e5d5c88174d15c8
doug24 <17227248+doug24@users.noreply.github.com>
2025-12-21 14:29:25 -0800

    1401 add special character handling for the tab character

---
+++ dnGREP.WPF/MVHelpers/SpecialCharacterToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace dnGREP.WPF
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class SpecialCharacterToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string text)
+            {
+                return text.Replace("\t", string.Empty, StringComparison.Ordinal);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
